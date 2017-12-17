9c4c589de303390af7dbac79f1024e9153f9590c
bacastu <baptiste.calin@student.umons.ac.be>
2017-12-17 11:35:40 +0100

    diag classe + activité

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("Main.fxml"));
         primaryStage.setTitle("TrainTicketDistributor");
         primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
